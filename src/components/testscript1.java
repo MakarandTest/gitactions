4c43ded3824b9778dd583e626d38d24a7e8cf1c6
MakarandTest <132062725+MakarandTest@users.noreply.github.com>
2023-05-11 15:25:31 +0530

    Update testscript1.java

--- src/components/testscript1.java
+++ src/components/testscript1.java
@@ -1,5 +1,7 @@
 class HelloWorld { 
   public static void main(String[] args) { 
+    String apiToken = ""asfadfd54545asfd65a;
+    String password = "1234";
     System. out. println("Hello World!"); // Hello World! 
   } 
 }
